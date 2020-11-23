72df183ee159d05bc2d8ade26da914e29257ff63
binaryprotagonist@gmail.com <binaryprotagonist@example.com>
2020-11-23 20:38:42 +0530

    frontend

--- src/app/core/utils/constants.ts
+++ src/app/core/utils/constants.ts
@@ -1,6 +1,6 @@
 export const constants = {
 
-    apiBaseURL: 'https://mvpnotes-backend.herokuapp.com/',
+    apiBaseURL: 'http://mvpnotes-backend.herokuapp.com/',
     //angularBaseURL:'http://34.219.216.136:9092/',
     headers: { 'accept': 'application/json', 'content-type': 'application/json' },
 
