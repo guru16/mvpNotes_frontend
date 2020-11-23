2d4f40e36dc1498ceea60fd220de72bbf0f78c2c
binaryprotagonist@gmail.com <binaryprotagonist@example.com>
2020-11-23 19:20:28 +0530

    some changes

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/angular-deploy'));
+app.use(express.static(__dirname + '/dist/mvp-notes'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-deploy/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/mvp-notes/index.html'));
 });
 
 // default Heroku PORT
