ba41f703d0ce0ff3cff98c9f29a4592ae520e2f0
binaryprotagonist@gmail.com <binaryprotagonist@example.com>
2020-11-23 19:43:42 +0530

    frontend

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/mvp-notes'));
+app.use(express.static(__dirname + '/dist/mvpNotes'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/mvp-notes/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/mvpNotes/index.html'));
 });
 
 // default Heroku PORT
