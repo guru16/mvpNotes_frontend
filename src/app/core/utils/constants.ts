60b8e08b1aa1f47ce03de60a6c158f5f447efdbd
binaryprotagonist@gmail.com <binaryprotagonist@example.com>
2021-01-05 23:48:15 +0530

    new cganges

--- src/app/core/utils/constants.ts
+++ src/app/core/utils/constants.ts
@@ -1,5 +1,4 @@
 export const constants = {
-
     apiBaseURL: 'https://mvpnotes-backend.herokuapp.com/',
     //angularBaseURL:'http://34.219.216.136:9092/',
     headers: { 'accept': 'application/json', 'content-type': 'application/json' },
